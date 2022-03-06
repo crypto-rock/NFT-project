7c2dadccb8b5ab164a5a2beb7bd4247eb335d20f
makotoblockchain <makotodevwork@gmail.com>
2022-03-06 14:52:20 +0900

    hashdice

---
+++ atari-casino/icicb-casino-hashdice/icicb-casino-hashdice-unity/Assets/scripts/JsonType.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JsonType
+{
+    public string userName;
+    public float betAmount;
+    public int stateFlag;
+    public float chance;
+    public int compareNum;
+    public string token;
+    public float amount;
+}
+
+public class ReceiveJsonObject
+{
+    public double amount;
+    public bool gameResult;
+    public float earnAmount;
+    public int randomNumber;
+    public string errMessage;
+    public ReceiveJsonObject()
+    {
+    }
+    public static ReceiveJsonObject CreateFromJSON(string data)
+    {
+        return JsonUtility.FromJson<ReceiveJsonObject>(data);
+    }
+}
